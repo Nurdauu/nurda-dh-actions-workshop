2ec7c1e662dc4eb40917455d1e3f5ef3101a740f
Nurdauu <129145486+Nurdauu@users.noreply.github.com>
2024-12-07 12:50:59 +0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,7 +36,7 @@ const Home: FunctionComponent = (): ReactElement => {
 
   return (
     <>
-      <Headline>All Rockets available</Headline>
+      <Headline>All Codes available</Headline>
       <OctoCollection
         octoItems={data ?? []}
         onToggleOctocat={handleToggleOctocat}
